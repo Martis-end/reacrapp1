9e5786db2ea8e472e9e59d60fb178effe3591141
Martis-end <stepanov.dns@gmail.com>
2023-01-25 22:30:32 +0300

    comit3

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,22 +4,55 @@ import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
+import { Component } from 'react';
+
 import './app.css';
 
-function App() {
-  return (
-    <div className="app">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'Johnf C.', salary: 8000, id: 1 },
+        { name: 'Alexx M.', salary: 3000, id: 2 },
+        { name: 'Alexx M.', salary: 3000, id: 3 },
+        { name: 'Carl W.', salary: 5000, id: 4 }
+      ]
+    }
+    this.maxId = 4;
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div className="app">
         <AppInfo />
 
         <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+          <SearchPanel />
+          <AppFilter />
         </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+
+        <EmployeesList data={this.state.data} 
+          onDelete={this.deleteItem}
+        />
+        <EmployeesAddForm />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,24 +1,62 @@
 import './employees-list-item.css';
+import { Component } from 'react';
 
-const EmployeesListItem = () => {
-    return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue="1000$"/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+        this.state = {
+            classli: false,
+            like: false
+        }
+    }
+    changeClassLi = () => {this.setState(({classli}) => ({classli: !classli}))}
+
+    changeClassLike = () => {
+        this.setState((state) => {
+            return { like: !this.state.like };
+        })
+    }
+
+    changerClassLi = (classli, classLi, newClass) => {
+        if (classli) {
+            classLi += newClass;
+            return classLi;
+        } else {return classLi}
+    }
+
+    render() {
+        const { name, salary, onDelete } = this.props;
+
+        const { classli, like } = this.state;
+
+        let classLi = "list-group-item d-flex justify-content-between";
+
+        classLi = this.changerClassLi(classli, classLi, ' increase')
+        classLi = this.changerClassLi(like, classLi, ' like')
+
+        return (
+            <li className={classLi}>
+                <span className="list-group-item-label"
+                    onClick={this.changeClassLike}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + "$"} />
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.changeClassLi}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button"
+                        className="btn-trash btn-sm "
+                        onClick={onDelete}>
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
